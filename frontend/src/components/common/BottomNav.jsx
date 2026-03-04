1038213c43a8ccce5e1ff37fbb1044aba95c143a
julia-lars <1215087664@qq.com>
2026-03-04 22:09:38 +0800

    底部状态栏边框删除

--- frontend/src/components/common/BottomNav.jsx
+++ frontend/src/components/common/BottomNav.jsx
@@ -29,7 +29,7 @@ const BottomNav = () => {
   ];
 
   return (
-    <div className="fixed bottom-0 left-0 right-0 bg-white shadow-md border-t border-gray-200 z-10">
+    <div className="fixed bottom-0 left-0 right-0 bg-white shadow-md z-10">
       <div className="flex justify-around items-center h-20 w-full px-[80px]">
         {navItems.map((item) => {
           const isActive = location.pathname === item.path;
